052dc2fab97a979b634a16a06b897cc2f8fedf38
Janet Mayher <mayherjanet@gmail.com>
2017-10-03 22:02:06 -0500

    templates complete-app not working

--- main.py
+++ main.py
@@ -5,8 +5,39 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True  
 
+@app.route("/validate", methods=['POST'])
+def validate():
+    username_add = request.form['username']
+    password_add = request.form['password']
+    verify_add = request.form['verify']
+
+    username_error = ""
+    password_error = ""
+    verify_error = ""
+
+    if username_add == "":
+        username_error= "User name cannot be blank." 
+        username_add = ""
+
+    else:
+        if len(username_add) < 3 or len(username_add) > 20:
+            username_error = "User name must have at least 3 characters and fewer than 20."
+            username_add = ""
+
+    if not username_error:
+        return redirect("/welcome")
+    else:
+        return render_template('home.html', username=username_add, username_error=username_error)
+ 
+@app.route("/welcome", methods=['POST'])
+def welcome():
+    username = request.form['username']
+    return render_template('welcome.html', username=username)
+
 
 @app.route("/")
+def index():
+    return render_template('home.html')
 
 
 app.run()
