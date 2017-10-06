58c3a613e270557658b0cbe0f2db7cb0bd8b7f26
Janet Mayher <mayherjanet@gmail.com>
2017-10-05 19:54:44 -0500

    everything works

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True  
 
-@app.route("/validate", methods=['POST'])
+@app.route("/", methods=['POST'])
 def validate():
     username_add = request.form['username']
     password_add = request.form['password']
@@ -44,28 +44,32 @@ def validate():
         password_add = ""
 
     #test password length
-    else:
-        if len(password_add) < 3 or len(password_add) >20:
+    elif len(password_add) < 3 or len(password_add) >20:
             password_error = "Password must have at least 3 characters and fewer than 20"
             password_add = ""
 
     #test if passwords match
-    if password_add != verify_add:
-        verify_error = "Passwords must match."
-        password_error = "Passwords must match."
+    else:
+        if password_add != verify_add:
+            verify_error = "Passwords must match."
+            password_error = "Passwords must match."
 
     #test for valid email
-    if '@' not in email_add or '.' not in email_add:
+    if email_add =="":
+        email_add = email_add
+
+    elif '@' not in email_add or '.' not in email_add:
         email_error = "Email is not valid. It must contain an '@' and a '.'"
         email_add = ""
 
-    if " " in email_add:
+    elif " " in email_add:
         email_error = "Emain is not valid. It cannot contain spaces."
         email_add = ""
 
-    if len(email_add) < 3 or len(email_add) > 20:
-        email_error = "Email is not valid. It must be between 3 and 20 characters long."
-        email_add = ""
+    else: 
+        if len(email_add) < 3 or len(email_add) > 20:
+            email_error = "Email is not valid. It must be between 3 and 20 characters long."
+            email_add = ""
 
     if not username_error and not password_error and not verify_error and not email_error:
         return render_template('welcome.html', username=username_add)
